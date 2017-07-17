4223fdee1ffdc5ab8232f958fedb4204a705936a
Boopathi Rajaa <me@boopathi.in>
2017-07-17 22:41:19 +0200

    update tests
    Update package.json files

--- test/cpa.test.js
+++ test/cpa.test.js
@@ -1,35 +1,19 @@
-jest.mock("chalk");
-
-const findMatches = require("../");
-const { parse } = require("babylon");
+const { findDuplicates, stringify } = require("../");
 const path = require("path");
 const fs = require("fs");
 
 const fixturesDir = path.join(__dirname, "fixtures");
 
-let result,
-  write = process.stdout.write,
-  columns = process.stdout.columns;
-
-beforeEach(() => {
-  result = "";
-  columns = 0;
-  process.stdout.write = msg => {
-    result += msg;
-  };
-});
-
-afterEach(() => {
-  process.stdout.write = write;
-  process.stdout.columns = columns;
-});
-
-test("should print no matches found", () => {
-  const noMatch = findMatches(path.join(fixturesDir, "nomatch.js"));
-  expect(result).toMatchSnapshot();
-});
-
-test("should print matches with largest subexpression in the tree", () => {
-  const match = findMatches(path.join(fixturesDir, "match.js"));
-  expect(result).toMatchSnapshot();
+describe("fixtures", () => {
+  fs.readdirSync(fixturesDir).forEach(name =>
+    test(name, () => {
+      const fixture = fs.readFileSync(path.join(fixturesDir, name)).toString();
+      expect(
+        stringify(findDuplicates(fixture, name), {
+          colors: false,
+          newline: false
+        })
+      ).toMatchSnapshot();
+    })
+  );
 });
