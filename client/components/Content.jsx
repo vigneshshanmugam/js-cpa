459d4278edef681d11a1ae7a6893489442941622
Boopathi Rajaa <me@boopathi.in>
2017-08-19 12:50:52 +0200

    Content: section now has separate state isActive instead of index based active

--- client/components/Content.jsx
+++ client/components/Content.jsx
@@ -5,136 +5,90 @@ import PrettyPrint from "./PrettyPrint";
 
 import styles from "./Content.css";
 
-class Content extends Component {
+export class Section extends Component {
   constructor(...args) {
     super(...args);
-
     this.state = {
-      activeItem: 0
+      isActive: this.props.idx === 0 ? true : false
     };
 
-    this.handleItemChange = this.handleItemChange.bind(this);
+    this.toggleActive = () => this.setState({ isActive: !this.state.isActive });
   }
+  render({ data, idx, baseLine, codeLength, codeThreshold }, { isActive }) {
+    const {
+      fileContent,
+      file,
+      id,
+      loc: { end: { line: endLine } = {}, start: { line: startLine } = {} } = {}
+    } = data;
+    let outputCode = fileContent;
+    let dataStart = baseLine;
+    let dataLine = `${startLine}-${endLine}`;
+    // If the code length is greater than threshold
+    // then print only the duplicate function and
+    // 5 lines before and after the duplicates
+    if (file.length > codeLength) {
+      let start = startLine - codeThreshold;
+      let end = endLine + codeThreshold;
 
-  componentDidUpdate(nextProps) {
-    if (this.shallowDiffers(this.props, nextProps)) {
-      this.setState({
-        activeItem: 0
-      });
-    }
-  }
-
-  shallowDiffers(a, b) {
-    for (let i in a) if (!(i in b)) return true;
-    for (let i in b) if (a[i] !== b[i]) return true;
-    return false;
-  }
-
-  handleItemChange(index, { el, targetEl }) {
-    if (index === 0) return;
-    this.setState(
-      {
-        activeItem: index
-      },
-      () => {
-        setTimeout(() => {
-          const { top } = el.getBoundingClientRect();
-          const { height } = targetEl.getBoundingClientRect();
-          window.scrollTo(0, top - height - 40);
-        }, 0);
+      // Reset the start if its less than 0
+      if (start < 0) {
+        start = 0;
       }
-    );
-  }
-
-  render(
-    {
-      data,
-      codeLength = 40, // Lines after which entire code should not be displayed
-      codeThreshold = 3, // Lines to show before and after the slice
-      baseLine = 1 // Starting line number
-    },
-    { activeItem }
-  ) {
-    if (!data || !data.length === 0) {
-      return;
+      // Reset the end if its greater than array length
+      if (end > file.length) {
+        end = file.length;
+      }
+      // Reset the starting line to the current start
+      dataStart = start;
+      // Reset the lines to highlight
+      dataLine = `${codeThreshold}-${codeThreshold + (endLine - startLine)}`;
+      // Save sliced array to outputCode
+      outputCode = file.slice(start, end).join("\n");
     }
-    return (
-      <div className={styles.content}>
-        <ul className={styles.printWrapper}>
-          {data.map((d, i) => {
-            const {
-              fileContent,
-              file,
-              id,
-              loc: {
-                end: { line: endLine } = {},
-                start: { line: startLine } = {}
-              } = {}
-            } = d;
-            let outputCode = fileContent;
-            let dataStart = baseLine;
-            let dataLine = `${startLine}-${endLine}`;
-            // If the code length is creater than threshold
-            // then print only the duplicate function and
-            // 5 lines before and after the duplicates
-            if (file.length > codeLength) {
-              let start = startLine - codeThreshold;
-              let end = endLine + codeThreshold;
 
-              // Reset the start if its less than 0
-              if (start < 0) {
-                start = 0;
-              }
-              // Reset the end if its greater than array length
-              if (end > file.length) {
-                end = file.length;
-              }
-              // Reset the starting line to the current start
-              dataStart = start;
-              // Reset the lines to highlight
-              dataLine = `${codeThreshold}-${codeThreshold +
-                (endLine - startLine)}`;
-              // Save sliced array to outputCode
-              outputCode = file.slice(start, end).join("\n");
-            }
-            const pretttPrintProps = Object.assign(
-              {},
-              {
-                dataStart,
-                dataLine,
-                className: cx("language-javascript", "line-numbers")
-              }
-            );
-            return (
-              <li
-                className={cx(styles.section, {
-                  [styles.mainPrint]: i === 0,
-                  [styles.active]: i === activeItem
-                })}
-                onClick={e => {
-                  this.handleItemChange(i, {
-                    targetEl: e.target,
-                    el: e.target.parentNode
-                  });
-                }}
-              >
-                <div className={styles.sectionHeader}>
-                  <h6 className={styles.printTitle}>
-                    File name: {id}
-                  </h6>
-                </div>
-                <div className={cx(styles.sectionBody, styles.mainPrintBody)}>
-                  <PrettyPrint {...pretttPrintProps}>
-                    {outputCode}
-                  </PrettyPrint>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+    return (
+      <li
+        className={cx(styles.section, {
+          [styles.active]: isActive
+        })}
+      >
+        <div className={styles.sectionHeader} onClick={this.toggleActive}>
+          <h6 className={styles.printTitle}>
+            File name: {id}
+          </h6>
+        </div>
+        <div className={cx(styles.sectionBody, styles.mainPrintBody)}>
+          <PrettyPrint
+            dataStart={dataStart}
+            dataLine={dataLine}
+            className={cx("language-javascript", "line-numbers")}
+          >
+            {outputCode}
+          </PrettyPrint>
+        </div>
+      </li>
     );
   }
 }
 
-export default Content;
+// Content
+export default ({
+  data = [],
+  codeLength = 40,
+  codeThreshold = 3,
+  baseLine = 1
+}) =>
+  <div className={styles.content}>
+    <ul className={styles.printWrapper}>
+      {data.map((d, idx) =>
+        <Section
+          data={d}
+          idx={idx}
+          codeLength={codeLength}
+          codeThreshold={codeThreshold}
+          baseLine={baseLine}
+        />
+      )}
+    </ul>
+  </div>;
